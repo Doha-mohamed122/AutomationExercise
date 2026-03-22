32a81d2cd93dd542b056854cca77b53ba042420c
Doha Mohamed <mdoha2236@gmail.com>
2026-03-22 15:54:04 +0200

    update signup test and feature

--- src/test/java/steps/SignupStepDefinition.java
+++ src/test/java/steps/SignupStepDefinition.java
@@ -1,9 +1,12 @@
 package steps;
 
 import io.cucumber.java.en.*;
+import io.cucumber.datatable.DataTable;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.*;
+
+import java.util.Map;
 
 import static org.testng.Assert.assertTrue;
 
@@ -14,10 +17,24 @@ public class SignupStepDefinition {
     private LoginSignupPage loginSignupPage;
     private AccountInformationPage accountInformationPage;
     private String email;
+    private String firstName;
+    private String lastName;
+    private String company;
+    private String address;
+    private String country;
+    private String state;
+    private String city;
+    private String zipcode;
+    private String mobile;
+    private String password;
+    private String day;
+    private String month;
+    private String year;
 
     @Given("user is on home page")
     public void user_is_on_home_page() {
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://automationexercise.com/");
         homePage = new HomePage(driver);
     }
@@ -33,34 +50,62 @@ public class SignupStepDefinition {
         accountInformationPage = loginSignupPage.signup(name, email);
     }
 
-    @When("user fills other information with password {string}")
-    public void user_fills_other_information_with_password(String password) {
-        accountInformationPage.fillAccountInformation(
-                "Mrs",
-                password,
-                "7", "November", "2003",
-                true,
-                false
-        );
+    @When("user fills all required information")
+    public void user_fills_all_required_information(DataTable dataTable) {
+        Map<String, String> userData = dataTable.asMap();
 
-        accountInformationPage.fillAddressInformation(
-                "Doha",
-                "Mohamed",
-                "personal",
-                "10th of Ramadan city",
-                "Canada",
-                "Cairo",
-                "Cairo",
-                "12345",
-                "01000000000"
-        );
+        fillAccountInformation(userData);
+
+        fillAddressInformation(userData);
 
         accountInformationPage.clickCreateAccount();
     }
 
+    private void fillAccountInformation(Map<String, String> userData) {
+
+        boolean isTitleMr = userData.get("title").equalsIgnoreCase("Mr");
+
+        password = userData.get("password");
+        day = userData.get("day");
+        month = userData.get("month");
+        year = userData.get("year");
+
+        accountInformationPage.fillAccountInformation(
+                String.valueOf(isTitleMr),
+                password,
+                day, month, year,
+                true,  //
+                false
+        );
+    }
+
+    private void fillAddressInformation(Map<String, String> userData) {
+        firstName = userData.get("firstName");
+        lastName = userData.get("lastName");
+        company = userData.get("company");
+        address = userData.get("address");
+        country = userData.get("country");
+        state = userData.get("state");
+        city = userData.get("city");
+        zipcode = userData.get("zipcode");
+        mobile = userData.get("mobile");
+        accountInformationPage.fillAddressInformation(
+                firstName,
+                lastName,
+                company,
+                address,
+                country,
+                state,
+                city,
+                zipcode,
+                mobile
+        );
+    }
+
     @Then("account should be created successfully for {string}")
     public void account_should_be_created_successfully_for(String name) {
-        assertTrue(homePage.getLoggedInUser().contains(name));
+        assertTrue(homePage.getLoggedInUser().contains(name),
+                "Account not created successfully for: " + name);
         driver.quit();
     }
 }
--- src/test/resources/features/signup.feature
+++ src/test/resources/features/signup.feature
@@ -7,11 +7,26 @@ Feature: Signup to AutomationExercise
     Given user is on home page
     When user navigates to signup page
     And user enters name "<name>" and email "<email>" to signup
-    And user fills other information with password "<password>"
+    And user fills all required information
+      | Field     | Value           |
+      | title     | <title>         |
+      | password  | <password>      |
+      | day       | <dob_day>       |
+      | month     | <dob_month>     |
+      | year      | <dob_year>      |
+      | firstName | <first_name>    |
+      | lastName  | <last_name>     |
+      | company   | <company>       |
+      | address   | <address>       |
+      | country   | <country>       |
+      | state     | <state>         |
+      | city      | <city>          |
+      | zipcode   | <zipcode>       |
+      | mobile    | <mobile>        |
     Then account should be created successfully for "<name>"
 
     Examples:
-      | name    | email                | password |
-      | doha    | dohaA80@gmail.com    | 123456   |
-      | Ismail  | Ismail80@gmail.com   | 123456   |
-      | mohamed | mohamedA80@gmail.com | 123456   |
+      | name    | email                    | title | password | dob_day | dob_month | dob_year | first_name | last_name | company  | address              | country | state | city  | zipcode | mobile      |
+      | mohamed | mohamedhii0@gmail.com    | Mrs   | 123456   | 7       | November  | 2003     | Doha       | Mohamed   | personal | 10th of Ramadan city | Canada  | Cairo | Cairo | 12345   | 01000000000 |
+      | Ismail  | Ismailhii0@gmail.com     | Mr    | 123456   | 15      | January   | 1995     | Ismail     | Ahmed     | tech     | Nasr City            | Canada  | Cairo | Cairo | 54321   | 01000000001 |
+      | mohamed | mohamedhelooo0@gmail.com | Mr    | 123456   | 22      | March     | 1990     | Mohamed    | Ali       | finance  | Maadi                | Canada  | Cairo | Cairo | 67890   | 01000000002 |
